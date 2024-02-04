4fb487d345bf87b013d8e58fff1a0fa98caa1d3b
A. Talha Ercan <109243387+ATErcan@users.noreply.github.com>
2024-02-04 15:15:39 +0100

    [feature/auth] - implement authentication (#2)

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import {
   Avatar,
   Dropdown,
@@ -10,14 +12,34 @@ import {
   NavbarLink,
   NavbarToggle,
 } from 'flowbite-react';
+import { useContext } from 'react';
+import { AuthContext } from './auth/AuthContext';
+import { logout } from './auth/authentication';
+import { useRouter } from 'next/navigation';
 
 export default function Nav() {
+  const { user } = useContext(AuthContext);
+  const router = useRouter();
+
+  const handleLogout = () => {
+    logout().then(result => {
+      if(result.success) {
+        router.push('/');
+        window.location.reload();
+      } else {
+        console.log(result.error);
+      }
+    }).catch((error) => {
+      console.log(error);
+    });
+  }
+
   return (
     <Navbar rounded>
-      <NavbarBrand href="https://flowbite-react.com">
-        <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Flowbite React</span>
+      <NavbarBrand href="https://github.com/ATErcan/todo-next">
+        <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">TODO APP</span>
       </NavbarBrand>
-      <div className="flex md:order-2">
+      {user && <div className="flex md:order-2">
         <Dropdown
           arrowIcon={false}
           inline
@@ -26,23 +48,30 @@ export default function Nav() {
           }
         >
           <DropdownHeader>
-            <span className="block text-sm">Bonnie Green</span>
-            <span className="block truncate text-sm font-medium">name@flowbite.com</span>
+            <span className="block text-sm">{user?.username}</span>
+            <span className="block truncate text-sm font-medium">{user?.email}</span>
           </DropdownHeader>
-          <DropdownItem>Dashboard</DropdownItem>
+          {/* <DropdownItem>Dashboard</DropdownItem>
           <DropdownItem>Settings</DropdownItem>
-          <DropdownItem>Earnings</DropdownItem>
-          <DropdownDivider />
-          <DropdownItem>Sign out</DropdownItem>
+          <DropdownItem>Earnings</DropdownItem> */}
+          {/* <DropdownDivider /> */}
+          <DropdownItem onClick={handleLogout}>Sign out</DropdownItem>
         </Dropdown>
         <NavbarToggle />
-      </div>
+      </div>}
       <NavbarCollapse>
         <NavbarLink href="/" active>
-          TODOS
+          Home
         </NavbarLink>
-        <NavbarLink href="/register">Register</NavbarLink>
-        <NavbarLink href="/login">Login</NavbarLink>
+        <NavbarLink href="/todos">TODOS</NavbarLink>
+        {
+          user ? 
+          <NavbarLink href="/add-todo">Add Todo</NavbarLink> :
+          <>
+            <NavbarLink href="/register">Register</NavbarLink>
+            <NavbarLink href="/login">Login</NavbarLink>
+          </>
+        }
       </NavbarCollapse>
     </Navbar>
   );
