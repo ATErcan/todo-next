a52b88cfc14aa19bfc86c83d6d0b1f57ed218365
talha <atercan0658@gmail.com>
2024-02-05 19:19:07 +0100

    [feature/edit-delete-todo] - add edit and delete functionality

--- components/todo/TodoItem.tsx
+++ components/todo/TodoItem.tsx
@@ -1,10 +1,38 @@
 import { Todo } from '@/lib/todo/types';
 import { AccordionContent, AccordionPanel, AccordionTitle } from 'flowbite-react';
 import React from 'react'
+import { deleteTodo, updateTodoStatus } from './todos';
+import { revalidatePath } from 'next/cache';
+import Link from 'next/link';
 
 export default function TodoItem({ todo }: { todo: Todo }): JSX.Element {
-  const completed = "";
-  const notCompleted = "";
+  const handleComplete = async (data: FormData) => {
+    'use server';
+    const id = data.get('id');
+    const status = data.get('status');
+    if(id && status) {
+      const result = await updateTodoStatus(id, status);
+      if(result.success) {
+        revalidatePath('/todos');
+      } else {
+        console.log(result.error);
+      }
+    }
+  }
+
+  const handleDelete = async (data: FormData) => {
+    'use server';
+    const id = data.get('todoId');
+    if(id){
+      const result = await deleteTodo(id);
+      if(result.success) {
+        revalidatePath('/todos')
+      } else {
+        console.log(result.error);
+      }
+    }
+  }
+
   return (
     <AccordionPanel>
       <AccordionTitle
@@ -17,17 +45,20 @@ export default function TodoItem({ todo }: { todo: Todo }): JSX.Element {
           {todo?.description || 'No description. Click to edit and add description.'}
         </p>
         <div className='todo-buttons flex flex-wrap text-center justify-center w-full h-full sm:justify-start'>
-          <div className='todo-container flex flex-col items-center justify-center text-center py-1.5 w-[135px]'>
-            <p className={`todo-btn effect01 ${todo.is_done ? "hover:bg-[#F44336]" : "hover:bg-[#4CAF50]"}`}>
+          <form action={handleComplete} className='todo-container flex flex-col items-center justify-center text-center py-1.5 w-[135px]'>
+            <input name="id" className="hidden" value={todo.id} readOnly />
+            <input name="status" className="hidden" value={`${todo.is_done}`} readOnly />
+            <button className={`todo-btn effect01 ${todo.is_done ? "hover:bg-[#F44336]" : "hover:bg-[#4CAF50]"}`}>
               <span>{todo.is_done ? "Not Completed" : "Completed"}</span>
-            </p>
-          </div>
+            </button>
+          </form>
           <div className='todo-container flex flex-col items-center justify-center text-center py-1.5 w-[135px]'>
-            <p className="todo-btn effect01 hover:bg-blue-600"><span>Edit</span></p>
+            <Link href={`/add-todo/${todo.id}`} className="todo-btn effect01 hover:bg-blue-600"><span>Edit</span></Link>
           </div>
-          <div className='todo-container flex flex-col items-center justify-center text-center py-1.5 w-[135px]'>
-            <p className="todo-btn effect01 hover:bg-zinc-950"><span>Delete</span></p>
-          </div>
+          <form action={handleDelete} className='todo-container flex flex-col items-center justify-center text-center py-1.5 w-[135px]'>
+            <input name="todoId" className="hidden" value={todo.id} readOnly />
+            <button className="todo-btn effect01 hover:bg-zinc-950" type='submit'><span>Delete</span></button>
+          </form>
         </div>
       </AccordionContent>
     </AccordionPanel>
--- components/todo/todos.ts
+++ components/todo/todos.ts
@@ -4,7 +4,7 @@ import { NewTodo, Todo } from "@/lib/todo/types";
 import axios from "axios";
 import { cookies } from "next/headers";
 
-export const getTodos = async () => {
+export const getAllTodos = async () => {
   const token = cookies().get('token');
   if(token) {
     const url = "http://127.0.0.1:8000/api/todos/";
@@ -47,6 +47,138 @@ export const newTodo = async (todo: NewTodo) => {
       if(response.status === 201) {
         const data: Todo = response.data;
         return { success: true, data: data };
+      } else {
+        const errorData = response.data;
+        return { success: false, error: errorData };
+      }
+    } catch (error) {
+      if(axios.isAxiosError(error)){
+        console.log(error.message);
+      } else {
+        console.log(error);
+      }
+      return { success: false, error: 'An error occurred' };
+    }
+  } else {
+    return { success: false, error: 'An authentication error occured'};
+  }
+}
+
+export const updateTodoStatus = async (id: FormDataEntryValue, status: FormDataEntryValue) => {
+  const token = cookies().get('token');
+  if(token) {
+    const url = `http://127.0.0.1:8000/api/todos/${id}/`;
+    try {
+      const response = await axios.patch(url, {
+        is_done: !(JSON.parse(status.toString()))
+      }, {
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Token ${token.value}`
+        }
+      })
+      if(response.status === 200) {
+        const data: Todo = response.data;
+        return { success: true, data: data };
+      } else {
+        const errorData = response.data;
+        return { success: false, error: errorData };
+      }
+    } catch (error) {
+      if(axios.isAxiosError(error)){
+        console.log(error.message);
+      } else {
+        console.log(error);
+      }
+      return { success: false, error: 'An error occurred' };
+    }
+  } else {
+    return { success: false, error: 'An authentication error occured'};
+  }
+}
+
+export const editTodo = async (id: string, todo: NewTodo) => {
+  const token = cookies().get('token');
+  if(token) {
+    const url = `http://127.0.0.1:8000/api/todos/${id}/`;
+    try {
+      const response = await axios.put(url, {
+        task: todo.task,
+        description: todo.description,
+        is_done: todo.completed
+      },
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Token ${token.value}`
+        }
+      })
+
+      if(response.status === 200) {
+        const data: Todo = response.data;
+        return { success: true };
+      } else {
+        const errorData = response.data;
+        return { success: false, error: errorData };
+      }
+    } catch (error) {
+      if(axios.isAxiosError(error)){
+        console.log(error.message);
+      } else {
+        console.log(error);
+      }
+      return { success: false, error: 'An error occurred' };
+    }
+  } else {
+    return { success: false, error: 'An authentication error occured'};
+  }
+}
+
+export const getTodo = async (id: string) => {
+  const token = cookies().get('token');
+  if(token) {
+    const url = `http://127.0.0.1:8000/api/todos/${id}/`;
+    try {
+      const response = await axios.get(url, {
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Token ${token.value}`
+        }
+      })
+
+      if(response.status === 200) {
+        const data: Todo = response.data;
+        return { success: true, data: data };
+      } else {
+        const errorData = response.data;
+        return { success: false, error: errorData }
+      }
+    } catch (error) {
+      if(axios.isAxiosError(error)){
+        console.log(error.message);
+      } else {
+        console.log(error);
+      }
+      return { success: false, error: 'An error occurred' };
+    }
+  } else {
+    return { success: false, error: 'An authentication error occured' };
+  }
+}
+
+export const deleteTodo = async (id: FormDataEntryValue) => {
+  const token = cookies().get('token');
+  if(token) {
+    const url = `http://127.0.0.1:8000/api/todos/${id}/`;
+    try {
+      const response = await axios.delete(url, {
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Token ${token.value}`
+        }
+      })
+      if(response.status === 204) {
+        return { success: true };
       } else {
         const errorData = response.data;
         return { success: false, error: errorData };
